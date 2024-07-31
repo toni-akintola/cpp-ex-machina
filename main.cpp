f6e220d59f0d28cc114385f476f722cf7770cbc2
Adarsh Gadepalli <adgadepalli2003@gmail.com>
2024-07-30 21:23:07 -0500

    configured eigen library

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "eigen-3.4.0/Eigen/Dense"
 
 int main() {
-    std::cout << 1 << std::endl;
+    Eigen::MatrixXd A(2, 3);
+    A << 1, 2, 4,
+         4, 5, 6;
+
+    Eigen::MatrixXd B(3, 2);
+    B << 7, 8,
+         9, 10,
+         11, 12;
+
+    Eigen::MatrixXd C = A * B;
+
+    std::cout  << C << std::endl;
+
     return 0;
 }
