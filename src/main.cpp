44e2a9bbf138289af71bb547780f3c000e13f1b5
Adarsh Gadepalli <adgadepalli2003@gmail.com>
2024-08-02 21:43:29 -0500

    added GeometricBrownianMotion model, implemented example in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
-#include <curl/curl.h>
-#include <Eigen/Dense>
+#include "GeometricBrownianMotion.h"
 
 int main() {
-    Eigen::MatrixXd A(2, 3);
-    A << 1, 2, 4,
-         4, 5, 6;
+    double p0 = 100.0;  
+    double mu = 0.05;  
+    double sigma = 0.2; 
+    double T = 1.0;   
+    int n = 10;         
 
-    Eigen::MatrixXd B(3, 2);
-    B << 7, 8,
-         9, 10,
-         11, 12;
+    GeometricBrownianMotion gbm(p0, mu, sigma, T, n);
 
-    Eigen::MatrixXd C = A * B;
-
-    std::cout  << C << std::endl;
+    Eigen::MatrixXd sim = gbm.MonteCarloSimulate(100);
+    
+    std::cout << "New value: " << sim << std::endl;
 
     return 0;
 }
